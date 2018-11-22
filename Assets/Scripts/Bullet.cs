ed3a389c35a0b98195fa89058a7ed0d3bf5cfaa4
OwenWheaton <43572923+OwenWheaton@users.noreply.github.com>
2018-11-22 09:29:40 +0000

    ZombieShooter155_2
    Weapon and bullet firing set up and functional.

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -2,15 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bullet : MonoBehaviour {
+public class Bullet : MonoBehaviour
+{
+    public float moveSpeed = 100.0f;
+    public int damage = 1;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    private void Start()
+    {
+        GetComponent<Rigidbody2D>().AddForce(transform.up * moveSpeed);
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        other.transform.SendMessage("TakeDamage", damage, SendMessageOptions.DontRequireReceiver);
+        Die();
+    }
+
+    private void OnBecameInvisible()
+    {
+        Die();
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
 }
+	
+
